3bf10c31be59b58405ac1f68a960bf4a9dd913fe
Pradhumansinh Padhiyar <pradhu619@gmail.com>
2023-12-04 12:18:31 +0530

    [Day-29] --Day-29_Challenge_Solution

---
+++ solution/day_29.sql
@@ -0,0 +1,1 @@
+SELECT Artist.Name AS ArtistName, SUM(InvoiceLine.UnitPrice * InvoiceLine.Quantity) AS TotalSales FROM Artist JOIN Album ON Artist.ArtistId = Album.ArtistId JOIN Track ON Album.AlbumId = Track.AlbumId JOIN InvoiceLine ON Track.TrackId = InvoiceLine.TrackId GROUP BY Artist.ArtistId ORDER BY TotalSales DESC LIMIT 5;
