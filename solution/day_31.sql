ddc93cc8cbc9ed3ffe6c1ba8215995da2c1c2834
Pradhumansinh Padhiyar <pradhu619@gmail.com>
2023-12-06 09:39:55 +0530

    [Day-31] --Day-31_Challenge_Solution

---
+++ solution/day_31.sql
@@ -0,0 +1,1 @@
+SELECT Artist.Name AS Name, COUNT(Album.AlbumId) AS AlbumCount FROM Artist LEFT JOIN Album ON Artist.ArtistId = Album.ArtistId GROUP BY Artist.ArtistId, Artist.Name ORDER BY Artist.ArtistId
