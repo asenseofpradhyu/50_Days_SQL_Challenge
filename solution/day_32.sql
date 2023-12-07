a1d26d012df90f721a38ac969e25c2517c812791
Pradhumansinh Padhiyar <pradhu619@gmail.com>
2023-12-07 09:30:34 +0530

    [Day-32] --Day-32_Challange_Solution

---
+++ solution/day_32.sql
@@ -0,0 +1,1 @@
+SELECT categories.category_name, ROUND(AVG(products.unit_price), 2) AS average_unit_price FROM categories JOIN products ON categories.category_id = products.category_id GROUP BY categories.category_id, categories.category_name ORDER BY average_unit_price DESC
