8c452ed1aabc8d9de60858aaffcf7ede2cf5e14c
Pradhumansinh Padhiyar <pradhu619@gmail.com>
2023-12-13 09:35:40 +0530

    [Day-38] --Day-38_Challenge_Solution

---
+++ solution/day_38.sql
@@ -0,0 +1,1 @@
+SELECT doctors.first_name, doctors.last_name, COUNT(admissions.patient_id) AS admissions_total FROM doctors JOIN admissions ON doctors.doctor_id = admissions.attending_doctor_id GROUP BY doctors.doctor_id, doctors.first_name, doctors.last_name
