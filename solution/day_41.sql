5d0f64350946c85fbe3713ca7b91f3e8d9451302
Pradhumansinh Padhiyar <pradhu619@gmail.com>
2023-12-16 21:57:11 +0530

    [Day-41] --Day-41_Challenge_Solution

---
+++ solution/day_41.sql
@@ -0,0 +1,1 @@
+SELECT admission_date, COUNT(admission_date) AS admission_day, COUNT(admission_date) - LAG(COUNT(admission_date)) OVER (ORDER BY admission_date) AS admission_count_change FROM admissions GROUP BY admission_date ORDER BY admission_date
