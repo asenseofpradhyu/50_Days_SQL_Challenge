2f602ffcb037e93c3617b03e56a8c039dcbdfb4c
Pradhumansinh Padhiyar <pradhu619@gmail.com>
2023-12-22 21:18:09 +0530

    [Day-47] --Day-47_Challenge_Solution

---
+++ solution/day_47.sql
@@ -0,0 +1,1 @@
+SELECT "Customer"."CustomerId", MAX("Invoice"."InvoiceDate") AS "MaxInvoiceDate" FROM "Customer" JOIN "Invoice" ON "Customer"."CustomerId" = "Invoice"."CustomerId" GROUP BY "Customer"."CustomerId" ORDER BY "Customer"."CustomerId" ASC
